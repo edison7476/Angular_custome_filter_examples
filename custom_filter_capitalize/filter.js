3067c1712bc6c3ca49735458beeb3f406a299c22
jia <edison7476@hotmail.com>
2016-11-07 21:58:38 -0800

    Custom filter for capitalizing the first letter.

---
+++ custom_filter_capitalize/filter.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', function($scope){
+  $scope.names = ['edison', 'kyle', 'frank', 'grant', 'cole'];
+});
+
+myApp.filter('capitalize', function(){
+  return function (str){
+    if(isNaN(str)){
+        var upperCase = str.charAt(0).toUpperCase();
+        var rest = str.slice(1,str.length);
+        var newStr = upperCase + rest;
+        // console.log(newStr);
+        return newStr;
+    }
+  };
+});
