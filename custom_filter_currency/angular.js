fa5f1ee87fb459069f2dd94184f17d41320c6096
jia <edison7476@hotmail.com>
2016-11-08 14:53:01 -0800

    custom filter - currency filter

--- custom_filter_currency/angular.js
+++ custom_filter_currency/angular.js
@@ -5,7 +5,7 @@ controller('myController', function ($scope){
   $scope.currencies = [100,88,76,90,124, 'Hello'];
 }).
 filter('customCurrency', function (){
-  return function (currency, symbol, back){
+  return function (currency, symbol){
       if(isNaN(currency)){
         return currency;
       }
